2dd0d1315eeeaf013db350490deb2d34c923a850
Eriber1 <111772796+Eriber1@users.noreply.github.com>
2023-11-20 23:26:10 -0600

    funciona el login con google

---
+++ src/assets/Components/Home.jsx
@@ -0,0 +1,17 @@
+const Home = () => {
+   //metodo de importar el auth context
+    //usando el Authcontext se puede acceder a los datos del padre que en este caso contiene informacion del usuario que esta puesto en true
+    // el que lo contiene es el context 
+    //const AuthContext = useContext(context)
+    //console.log(AuthContext)
+
+    //metodo del hook en dode no se tiene que importar y solo se llama a la funcion 
+    //const {user} = useAuth()
+    //console.log(user)
+
+    return(
+        <div>hola</div>
+    )
+}
+
+export default Home;
