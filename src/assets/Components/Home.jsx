85bf22f97b8deae01869d58fa30226a2d2174b2a
Eriber1 <111772796+Eriber1@users.noreply.github.com>
2023-11-28 12:21:52 -0600

    home

--- src/assets/Components/Home.jsx
+++ src/assets/Components/Home.jsx
@@ -1,17 +1,51 @@
+const buttonGroupStyle = {
+  display: "flex",
+  flexWrap: "wrap",
+  justifyContent: "space-around",
+};
+
+const buttonContainerStyle = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  margin: "60px",
+};
+
+const smallerButtonStyle = {
+    fontSize: '10px', // Puedes ajustar este valor según tu preferencia
+  };
+
 const Home = () => {
-   //metodo de importar el auth context
-    //usando el Authcontext se puede acceder a los datos del padre que en este caso contiene informacion del usuario que esta puesto en true
-    // el que lo contiene es el context 
-    //const AuthContext = useContext(context)
-    //console.log(AuthContext)
+  return (
+    <>
+      <div style={buttonGroupStyle}>
+        <div className="card" style={buttonContainerStyle}>
+          <button className="btn btn-primary mb-3">+ Facebook</button>
+          <button className="btn btn-primary mb-3">+ X</button>
+          <button className="btn btn-primary mb-3">+ Youtube</button>
+          <button className="btn btn-primary mb-3">+ TikTok </button>
+          <button className="btn btn-primary mb-3">+ Whatsapp</button>
+          <button className="btn btn-danger mb-3">Eliminar</button>
+        </div>
 
-    //metodo del hook en dode no se tiene que importar y solo se llama a la funcion 
-    //const {user} = useAuth()
-    //console.log(user)
+        <div className="card" style={buttonContainerStyle}>
+          <button className="btn btn-primary mb-1" style={smallerButtonStyle}>+ Facebook</button>
+          <button className="btn btn-primary mb-1 " style={smallerButtonStyle}>+ X</button>
+          <button className="btn btn-primary mb-1" style={smallerButtonStyle}>+ Youtube</button>
+          <button className="btn btn-primary mb-1" style={smallerButtonStyle}>+ TikTok </button>
+          <button className="btn btn-primary mb-1" style={smallerButtonStyle}>+ Whatsapp</button>
+          <button className="btn btn-danger mb-1" style={smallerButtonStyle}>Eliminar</button>
+        </div>
+      </div>
+        <hr className="border-danger border-2 opacity-100 mb-3" />
 
-    return(
-        <div>hola</div>
-    )
-}
+        <div className="card">
+            <p>
+                Editor
+            </p>
+        </div>
+    </>
+  );
+};
 
 export default Home;
