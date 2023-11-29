ab0e8c6cc441526943ecefb4e74d012d0464446a
Eriber1 <111772796+Eriber1@users.noreply.github.com>
2023-11-29 12:15:37 -0600

    agregar material

--- src/assets/Components/Home.jsx
+++ src/assets/Components/Home.jsx
@@ -1,43 +1,35 @@
-const buttonGroupStyle = {
-  display: "flex",
-  flexWrap: "wrap",
-  justifyContent: "space-around",
-};
+import Button from "@mui/material/Button"; 
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 
-const buttonContainerStyle = {
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  margin: "60px",
-};
 
-const smallerButtonStyle = {
-    fontSize: '10px', // Puedes ajustar este valor según tu preferencia
-  };
 
 const Home = () => {
   return (
     <>
-      <div style={buttonGroupStyle}>
-        <div className="card" style={buttonContainerStyle}>
-          <button className="btn btn-primary mb-3">+ Facebook</button>
+       <div className="row">
+        <div className="col-sm-8 mt-10">
+          <div className="card mr-10 ml-11">
+            <div className="card-body btn-group-vertical">
+            <button className="btn btn-primary mb-3">+ Facebook</button>
           <button className="btn btn-primary mb-3">+ X</button>
           <button className="btn btn-primary mb-3">+ Youtube</button>
           <button className="btn btn-primary mb-3">+ TikTok </button>
           <button className="btn btn-primary mb-3">+ Whatsapp</button>
           <button className="btn btn-danger mb-3">Eliminar</button>
+            </div>
+          </div>
         </div>
-
-        <div className="card" style={buttonContainerStyle}>
-          <button className="btn btn-primary mb-1" style={smallerButtonStyle}>+ Facebook</button>
-          <button className="btn btn-primary mb-1 " style={smallerButtonStyle}>+ X</button>
-          <button className="btn btn-primary mb-1" style={smallerButtonStyle}>+ Youtube</button>
-          <button className="btn btn-primary mb-1" style={smallerButtonStyle}>+ TikTok </button>
-          <button className="btn btn-primary mb-1" style={smallerButtonStyle}>+ Whatsapp</button>
-          <button className="btn btn-danger mb-1" style={smallerButtonStyle}>Eliminar</button>
+        
+        <div className="col-sm-4 mt-6" >
+          <div className="container ">
+          <Button variant="contained" size="small" startIcon={<AddCircleIcon  />}>Agregar</Button>
+          <Button className="m-3" variant="contained" color="error" size="small" startIcon ={<DeleteIcon />}>Eliminar</Button>
+            </div>
         </div>
       </div>
-        <hr className="border-danger border-2 opacity-100 mb-3" />
+
+        <hr className="border-danger border-2 opacity-100 mb-3 mt-3" />
 
         <div className="card">
             <p>
